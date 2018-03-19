382d9dfb3c50117d1a7a29a1ec9c9b9e8bc13e94
peterzheng98 <33046323+peterzheng98@users.noreply.github.com>
2018-03-20 07:59:04 +0800

    Backup

---
+++ cmake-build-debug/CMakeFiles/2018DataStructureBigWork.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/2018DataStructureBigWork.dir/main.cpp.o"
+  "2018DataStructureBigWork.pdb"
+  "2018DataStructureBigWork"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2018DataStructureBigWork.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/2018DataStructureBigWork.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 2018DataStructureBigWork.
+# This may be replaced when dependencies are built.
